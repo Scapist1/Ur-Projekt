fe31fe3d92db4a005057c25acfac0d87d36951f0
Scapist1 <jaco6jaco6@gmail.com>
2026-04-09 09:58:09 +0200

    noter

--- src/timer.c
+++ src/timer.c
@@ -9,11 +9,19 @@ uint8_t ss = 0, mm = 0, hh = 0;
 void timer1_init() {
     TCCR1B |= (1 << WGM12);     // Timer 1, mode 4: CTC mode (reset timer ved bestemt værdi, så vi kan synkronisere timer med et sekund f.eks.)
     
+<<<<<<< HEAD
     OCR1A = OCR_VALUE;          // compileren udregner  og indstiller at timer skal reset hvert 249. ticks (ms)
+=======
+    OCR1A = OCR_VALUE;          // compileren udregner og indstiller at timer skal reset hvert 249 ticks (ms) (Output Compare Register)
+>>>>>>> ac4dd57 (noter)
     
     TIMSK1 |= (1 << OCIE1A);    // Giver timeren lov til at afbryde CPU'en, når den rammer værdien i OCR1A.
     TCCR1B |= (1 << CS11) | (1 << CS10); // Prescaler 64
 }
+
+// The Counter (TCNT1): When you set the prescaler bits in TCCR1B |= (1 << CS11) | (1 << CS10);, the timer starts. The hardware register TCNT1 begins counting up from 0.
+// The Match: Inside the AVR chip, there is a digital comparator. Every time TCNT1 increments, the chip asks: Is TCNT1 == OCR1A?
+
 
 // I servicerutinen tælles en global variabel op hver gang, der er interrupt – når værdien er 999,
 // sættes et flag = 1, og tællerværdien sættes til 0.
@@ -23,3 +31,4 @@ ISR(TIMER1_COMPA_vect) {  // Interrupt service routine hvert ms
       ss_flag = 1; // "der er gået mindst et sekund flaget!"
   }
 }
+
